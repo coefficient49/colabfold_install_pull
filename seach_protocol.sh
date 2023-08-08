3ce448b922ca3961fface50f54362915c3654454
Jeron Chen <jeronchen@gmail.com>
2023-08-08 00:27:20 -0400

    Create seach_protocol.sh

---
+++ seach_protocol.sh
@@ -0,0 +1,25 @@
+## getting file from s3
+FILE=`aws s3 ls s3://jchen-af-storage/fastas_new/ | awk 'length($4) >0 {print$4}' | head -n 1`
+
+## downloading file from s3
+aws s3 cp s3://jchen-af-storage/fastas_new/$FILE fastas/$FILE
+
+## transfering downloaded file from the _new folder to the _done folder
+
+aws s3 mv s3://jchen-af-storage/fastas_new/$FILE s3://jchen-af-storage/fastas_done/$FILE
+
+BASENAME=`basename $FILE .fasta`
+
+## fixing spacing issue in the name
+sed -i -e 's/> />/g' fastas/$FILE
+
+## run msa geneation
+colabfold_search fastas/$FILE ~/db/ msas/$BASENAME --db1 uniref30_2302_db --db3 colabfold_envdb_202108_db --db-load-mode 2
+
+
+## fix naming issue
+mv msas/$BASENAME/0.a3m msas/$BASENAME/$BASENAME.a3m
+
+
+## upload to s3 for another ec2 instance to process 
+aws s3 mv msas/$BASENAME/$BASENAME.a3m s3://jchen-af-storage/msa_new/$BASENAME.a3m
