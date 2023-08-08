64c6f3832035e436b9e213cb69b7c62a22a7b7e6
Jeron Chen <jeronchen@gmail.com>
2023-08-08 01:26:45 -0400

    Create folding_protocol.sh

---
+++ folding_protocol.sh
@@ -0,0 +1,34 @@
+FILESLEFT=`aws s3 ls jchen-af-storage/msa_new/ | awk 'length($4)>0 {print$4}' | wc -l`
+
+while (($FILESLEFT > 0))
+do
+
+## getting file from s3
+FILE=`aws s3 ls s3://jchen-af-storage/msa_new/ | awk 'length($4) >0 {print$4}' | head -n 1`
+
+## downloading file from s3
+aws s3 cp s3://jchen-af-storage/msa_new/$FILE msas/$FILE
+
+## transfering downloaded file from the _new folder to the _done folder
+
+aws s3 mv s3://jchen-af-storage/msa_new/$FILE s3://jchen-af-storage/msa_done/$FILE
+
+BASENAME=`basename $FILE .a3m`
+
+
+## run msa geneation
+colabfold_batch  --model-type alphafold2_multimer_v3  msas/$FILE msas/$BASENAME
+
+
+## fix naming issue
+mv msas/$BASENAME/0.a3m msas/$BASENAME/$BASENAME.a3m
+
+
+## upload to s3 for another ec2 instance to process 
+aws s3 mv pdbs/$BASENAME/ s3://jchen-af-storage/pdbs/$BASENAME --recursive
+
+
+FILESLEFT=`aws s3 ls jchen-af-storage/msa_new/ | awk 'length($4)>0 {print$4}' | wc -l`
+
+done
+
