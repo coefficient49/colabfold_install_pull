546bcc74834bee25f5b38ff28c6cdacbca98b80c
Jeron Chen <jeronchen@gmail.com>
2023-08-09 11:46:32 -0400

    Update upload_strays.sh

--- upload_strays.sh
+++ upload_strays.sh
@@ -3,5 +3,5 @@ do
   echo $ff
   filename=$(basename -- "$ff")
   echo $filename
-  aws s3 mv $ff s3://jchen-af-storage/$filename/ --recursive
+  aws s3 mv $ff s3://jchen-af-storage/pdbs/$filename/ --recursive
 done
