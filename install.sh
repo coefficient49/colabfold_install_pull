d01f6f39c9e7d8b7b09a814f3b17acf816f4ed1e
Jeron Chen <jeronchen@gmail.com>
2023-08-17 13:22:24 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,6 +18,9 @@ conda create -p $COLABFOLDDIR/colabfold-conda python=3.10 -y
 conda activate $COLABFOLDDIR/colabfold-conda
 conda update -n base conda -y
 conda install -c conda-forge python=3.10 cudnn==8.2.1.32 cudatoolkit==11.6.0 openmm==7.7.0 pdbfixer -y
+
+conda install -c conda-forge vmtouch -y
+
 # Download the updater
 wget -qnc https://raw.githubusercontent.com/YoshitakaMo/localcolabfold/main/update_linux.sh --no-check-certificate
 chmod +x update_linux.sh
