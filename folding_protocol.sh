cff8afe7ccc3a6c15595d3127a1354ccd15691ea
Jeron Chen <jeronchen@gmail.com>
2023-08-24 09:50:23 -0400

    updating folding protocol to full from project folders

--- folding_protocol.sh
+++ folding_protocol.sh
@@ -1,17 +1,18 @@
-FILESLEFT=`aws s3 ls jchen-af-storage/msa_new/ | awk 'length($4)>0 {print$4}' | wc -l`
+AWSFOLDER="$1"
+FILESLEFT=`aws s3 ls jchen-af-storage/$AWSFOLDER/msa_new/ | awk 'length($4)>0 {print$4}' | wc -l`
 
 while (($FILESLEFT > 0))
 do
 
 ## getting file from s3
-FILE=`aws s3 ls s3://jchen-af-storage/msa_new/ | awk 'length($4) >0 {print$4}' | head -n 1`
+FILE=`aws s3 ls s3://jchen-af-storage/$AWSFOLDER/msa_new/ | awk 'length($4) >0 {print$4}' | head -n 1`
 
 ## downloading file from s3
-aws s3 cp s3://jchen-af-storage/msa_new/$FILE msas/$FILE
+aws s3 cp s3://jchen-af-storage/$AWSFOLDER/msa_new/$FILE msas/$FILE
 
 ## transfering downloaded file from the _new folder to the _done folder
 
-aws s3 mv s3://jchen-af-storage/msa_new/$FILE s3://jchen-af-storage/msa_done/$FILE
+aws s3 mv s3://jchen-af-storage/$AWSFOLDER/msa_new/$FILE s3://jchen-af-storage/$AWSFOLDER/msa_done/$FILE
 
 BASENAME=`basename $FILE .a3m`
 
@@ -25,10 +26,10 @@ mv msas/$BASENAME/0.a3m msas/$BASENAME/$BASENAME.a3m
 
 
 ## upload to s3 for another ec2 instance to process 
-aws s3 mv msas/$BASENAME/ s3://jchen-af-storage/pdbs/$BASENAME --recursive
+aws s3 mv msas/$BASENAME/ s3://jchen-af-storage/$AWSFOLDER/pdbs/$BASENAME --recursive
 
 
-FILESLEFT=`aws s3 ls jchen-af-storage/msa_new/ | awk 'length($4)>0 {print$4}' | wc -l`
+FILESLEFT=`aws s3 ls jchen-af-storage/$AWSFOLDER/msa_new/ | awk 'length($4)>0 {print$4}' | wc -l`
 
 done
 
