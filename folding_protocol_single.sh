959f7072681be2b3fca81b94ea3a76e58e087a76
Jeron Chen <jeronchen@gmail.com>
2023-08-28 17:38:04 -0400

    commenting inputs

--- folding_protocol_single.sh
+++ folding_protocol_single.sh
@@ -12,20 +12,22 @@ aws s3 cp $AWSFILE msas/$FILE
 
 ## transfering downloaded file from the _new folder to the _done folder
 
-# aws s3 mv $AWSFILE s3://jchen-af-storage/$AWSFOLDER/msa_done/$FILE
+aws s3 mv $AWSFILE s3://jchen-af-storage/$AWSFOLDER/msa_done/$FILE
 
 BASENAME=`basename $FILE .a3m`
 
 
+
+
 ## run msa geneation
 # colabfold_batch  --templates --num-seeds 2 --model-type alphafold2_multimer_v3  msas/$FILE msas/$BASENAME
-# colabfold_batch  --templates --num-seeds 2 --model-type alphafold2_multimer_v3  --save-recycles --num-recycle $CYCLES  msas/$FILE msas/$BASENAME 
+colabfold_batch  --templates --num-seeds 2 --model-type alphafold2_multimer_v3  --save-recycles --num-recycle $CYCLES  msas/$FILE msas/$BASENAME 
 
 ## fix naming issue
-# mv msas/$BASENAME/0.a3m msas/$BASENAME/$BASENAME.a3m
+mv msas/$BASENAME/0.a3m msas/$BASENAME/$BASENAME.a3m
 
 
 ## upload to s3 for another ec2 instance to process 
-# aws s3 mv msas/$BASENAME/ s3://jchen-af-storage/$AWSFOLDER/pdbs/$BASENAME --recursive
+aws s3 mv msas/$BASENAME/ s3://jchen-af-storage/$AWSFOLDER/pdbs/$BASENAME --recursive
 
 
